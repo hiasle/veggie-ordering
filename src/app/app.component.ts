cb9f83c8cd6bfcb4908fb0e0b69d8588abf40e1f
Matthias Huber <matthias.hube@gmail.com>
2025-01-19 17:16:51 +0100

    angular 19 fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,39 @@
-import { BreakpointObserver } from '@angular/cdk/layout';
-import {
-  Component,
-  ViewChild,
-} from '@angular/core';
-import { MatSidenav } from '@angular/material/sidenav';
+import {BreakpointObserver} from '@angular/cdk/layout';
+import {Component, ViewChild,} from '@angular/core';
+import {MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
+import {CommonModule} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatListModule} from "@angular/material/list";
+import {RouterOutlet} from "@angular/router";
+import {DummyComponent} from "./dummy/dummy.component";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterOutlet,
+    DummyComponent
+  ],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'material-responsive-sidenav';
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
-  isMobile= true;
+  isMobile = true;
   isCollapsed = true;
 
 
-  constructor(private observer: BreakpointObserver) {}
+  constructor(private observer: BreakpointObserver) {
+  }
 
   ngOnInit() {
     this.observer.observe(['(max-width: 800px)']).subscribe((screenSize) => {
@@ -29,7 +43,7 @@ export class AppComponent {
   }
 
   toggleMenu() {
-    if(this.isMobile){
+    if (this.isMobile) {
       this.sidenav.toggle();
       this.isCollapsed = false;
     } else {
