11754e514c5e691149991841f73695cdb24c36ba
Matthias Huber <matthias.hube@gmail.com>
2025-01-19 16:12:58 +0100

    upgrade angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,10 @@ import {
 import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: false
 })
 export class AppComponent {
   title = 'material-responsive-sidenav';
